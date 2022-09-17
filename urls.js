ffd533ced3e60cb11b017803517a2055eda72d9d
Craig Chick <craigchick@outlook.com>
2022-09-17 23:20:47 +0200

    Front

--- urls.js
+++ urls.js
@@ -2,6 +2,6 @@
 const BACKEND_URL = 'https://weatherapp-part4-backend.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://weatherapp-part4-frontend.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
